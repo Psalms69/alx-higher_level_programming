ee15db812639b3254351dc3c58c68e36a426a57f
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-11-25 13:58:41 -0800

    8and 9 done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     last = number%10
     if number < 0:
-        last *= -1
+        number *= -1
     else:
          last = last
     print("{:d}".format(last), end ="")
