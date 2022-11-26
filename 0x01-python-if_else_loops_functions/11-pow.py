84c9675848960986086fabd04d16e634d70a45f6
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-11-26 06:20:46 -0800

    just be normal

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    exe = 0
-    if a < 0:
-        a *= -1
-    exe = 1
-    pow = a ^ b
-    if exe == 1:
-        pow = a ^ b
+    pow = a ** b
     return pow
