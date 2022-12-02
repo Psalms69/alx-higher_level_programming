207807ea9ebc0e990feca36a7edd76b7166a53be
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-12-02 06:57:53 -0800

    used for a file not module or package

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-from  variable_load_5.py import a
-
-def print_var() :
-    print("{}".format( a))
-
-
-
-if __name__ = "__main__":
-    print_var()
-
+import  variable_load_5
+new = variable_load_5.a
+def print_var(new) :
+    print("{}".format(new))
