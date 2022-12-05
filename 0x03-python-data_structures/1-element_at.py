7a66b8f5757d2723f455997b2faacca8d8144ab2
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-12-05 05:32:07 -0800

    fixed the signs  <>

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = len(my_list)
-    for y in idx:
-        if (idx <= 0):
-            return "none"
-        elif (idx > i):
-            return "none"
+    if (idx < 0):
+        return "none"
+    elif (idx > i):
+        return "none"
+    else:
+        return my_list[idx]
