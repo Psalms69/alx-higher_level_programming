d72d6c2815443ccf577671eb6a9a5b5be47726ee
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-12-05 05:16:48 -0800

    second try

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     i = len(my_list)
     for y in idx:
-        if (idx < 0):
-            return none
+        if (idx <= 0):
+            return "none"
         elif (idx > i):
-            return none
+            return "none"
