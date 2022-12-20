3dbd36865f214ff2c9e6dbecab68c9333cc5bb3d
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-12-20 14:43:54 -0800

    all

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
        max_value = None
        for number in my_list :
            if max_value is None or max_value < number:
-               lmax_value = number
+               max_value = number
                print(max_value)
