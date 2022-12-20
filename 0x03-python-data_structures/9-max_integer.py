a8389474de7a11b479f1e6eb180ae72c47f1c926
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-12-20 12:05:32 -0800

    first trial

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+       max_value = None
+       for number in my_list :
+           if max_value is None or max_value < number:
+               lmax_value = number
+               print(max_value)
